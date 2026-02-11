720ed95a23bfa57f9795b56af11c030b3cd41609
sudhanv09 <mystogun125@gmail.com>
2026-02-11 13:46:24 +0800

    cleanup

--- .config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-require 'core'
-
-vim.cmd.colorscheme("catppuccin")
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
